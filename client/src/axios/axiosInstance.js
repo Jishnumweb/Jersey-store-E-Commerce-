2745c06398fc9e480be12ee25a4e6840c9f654cc
jishnum <jishnuum2000@gmail.com>
2025-04-22 22:33:31 +0530

    error fixing

--- client/src/axios/axiosInstance.js
+++ client/src/axios/axiosInstance.js
@@ -1,12 +1,16 @@
-import axios from "axios";
+import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_BASEURL
+    baseURL : import.meta.env.VITE_BASEURL,
+    withCredentials:true
 })
-
 
 axiosInstance.interceptors.request.use((request) => {
     const token = localStorage.getItem("token")
     request.headers.Authorization = `Bearer ${token}`
     return request;
 })
+
+export default axiosInstance
+
+
