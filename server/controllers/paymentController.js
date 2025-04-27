95c9f9d4ca54b7b4cca34ee97bc84f8e104143a0
jishnum <jishnuum2000@gmail.com>
2025-04-27 19:24:26 +0530

    stripe updated

--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -2,13 +2,11 @@ const Stripe = require('stripe')
 
 const stripe = new Stripe(process.env.STRIPE_KEY)
 
-
 const paymentFunction = async (req, res) => {
     try {
         const userId = req.user
-        const { products,shippingAddress } = req.body
-        console.log(products,"cart items from checkout page");
-        
+        const { products, shippingAddress } = req.body
+        console.log(products, "cart items from checkout page");
 
         const lineItems = products.map((product) => ({
             price_data: {
@@ -16,35 +14,34 @@ const paymentFunction = async (req, res) => {
                 product_data: {
                     name: product.productId.title,
                     images: [product.productId.image]
-                    
                 },
-                
                 unit_amount: Math.round(product.productId.price * 100),
             },
             quantity: 1
-        }))
-        
+        }));
 
-        
+        // Create Stripe Checkout Session
         const session = await stripe.checkout.sessions.create({
             payment_method_types: ['card'],
             line_items: lineItems,
             mode: 'payment',
-            success_url: `${process.env.FRONTEND_URL}/payment/success`,
-            cancel_url: `${process.env.FRONTEND_URL}/payment/success`,
+            success_url: `${process.env.FRONTEND_URL}/payment/success?session_id={CHECKOUT_SESSION_ID}`,  // Add session_id to the URL
+            cancel_url: `${process.env.FRONTEND_URL}/payment/cancel`,
             metadata: {
                 userId: userId,  // Include user ID in metadata
                 shippingAddress: JSON.stringify(shippingAddress),  // Include shipping address in metadata
             }
         });
 
+        // Return session ID in response
         res.status(200).json({
-            success: true, sessionId: session.id
-        })
+            success: true,
+            sessionId: session.id
+        });
+
     } catch (error) {
         console.log(error);
         res.status(error.status || 500).json({ error: error.message || "Internal server error" })
-
     }
 }
 
